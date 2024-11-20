698eb9898bb040351762e5755ff833d8e10b468e
Oscar Cao <oscarcao777@gmail.com>
2024-11-20 14:20:33 +0000

    added test cases for initializing the ColourTable and implemented the methods

--- src/main/java/org/example/ColourTable.java
+++ src/main/java/org/example/ColourTable.java
@@ -1,4 +1,31 @@
 package org.example;
 
 public class ColourTable {
+    private final int[] colours;
+    private final int capacity;
+
+    public ColourTable(int capacity) {
+        if (capacity <= 1) {
+            throw new IllegalArgumentException("Capacity must be greater than 1.");
+        }
+        int test_capacity = capacity;
+        while (test_capacity > 1) {
+            if (test_capacity % 2 != 0) {
+                throw new IllegalArgumentException("Capacity must be a power of 2.");
+            }
+            test_capacity = test_capacity / 2;
+        }
+        this.colours = new int[capacity];
+        this.capacity = capacity;
+    }
+
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public int[] getColours() {
+        return this.colours;
+    }
+
 }
--- src/test/java/ColourTableTest.java
+++ src/test/java/ColourTableTest.java
@@ -1,4 +1,56 @@
-package PACKAGE_NAME;
+import org.example.ColourTable;
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for ColourTable Class.
+ */
 public class ColourTableTest {
+    /**
+     * Tests a valid constructor. Should not result in any errors.
+     */
+    @Test
+    void testValidConstructor() {
+        assertDoesNotThrow(() -> new ColourTable(2));
+    }
+
+    /**
+     * Tests an invalid constructor. Should result in IllegalArgumentException Error.
+     */
+    @Test
+    void testInvalidConstructorInvalidCapacity() {
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(-1));
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(0));
+    }
+
+    /**
+     * Tests an invalid constructor. Should result in IllegalArgumentException Error.
+     */
+    @Test
+    void testInvalidConstructorNonPowerOfTwoCapacity() {
+        assertThrows(IllegalArgumentException.class, () -> new ColourTable(5));
+    }
+
+    /**
+     * Tests getter method for capacity
+     */
+    @Test
+    void testGetCapacity() {
+        ColourTable table = new ColourTable(2);
+        int actualCapacity = table.getCapacity();
+        assertEquals(actualCapacity,2);
+    }
+
+    /**
+     * Tests getter method for colours
+     */
+    @Test
+    void testGetColours() {
+        ColourTable table = new ColourTable(2);
+        int[] expectedColours = {0,0};
+        int[] actualColours = table.getColours();
+        assertArrayEquals(expectedColours,actualColours);
+    }
+
 }
